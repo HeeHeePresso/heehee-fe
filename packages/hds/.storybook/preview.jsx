bc2ef24a7e3cd4507c33cbda747d1a80faf18b5b
narip <narip@github.com>
2024-03-16 17:42:50 +0900

    feat: 뷰포트 추가, 기본 뷰포트 설정

--- packages/hds/.storybook/preview.jsx
+++ packages/hds/.storybook/preview.jsx
@@ -1,10 +1,15 @@
 import { hakgoansimMulgyeolB, pretendard, caniculeDisplayB } from '@/systems/fonts';
 import '@/styles/reset.css';
+import { INITIAL_VIEWPORTS, MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
+    viewport: {
+      viewports: { ...INITIAL_VIEWPORTS, ...MINIMAL_VIEWPORTS },
+      defaultViewport: 'iphone14',
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
