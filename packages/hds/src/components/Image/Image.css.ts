ec22cbca0c95a251ffd6b130d128d4b3f2bc7801
sinamong0620 <sinaskim60@gmail.com>
2024-05-07 23:23:35 +0900

    #11 design : css 수정
    object-fit : cover, background-color: gray 추가

--- packages/hds/src/components/Image/Image.css.ts
+++ packages/hds/src/components/Image/Image.css.ts
@@ -1,23 +1,20 @@
-import { style} from '@vanilla-extract/css';
+import { style } from '@vanilla-extract/css';
 
-export const container = style({
-   width: '100%',          
- });
- 
- export const imageStyle = style({
-   width: '100%',          
-   height: 'auto',         
-   backgroundColor : "black",
- });
- 
- export const ratios = {
-   '16/9': style({
-     aspectRatio : "16/9"
-   }),
-   '3/4': style({
-      aspectRatio : "3/4"
-   }),
-   '1/1': style({
-      aspectRatio : "1/1"
-   }),
- };
+export const imageStyle = style({
+  width: '100%',
+  height: 'auto',
+  objectFit: 'cover',
+  backgroundColor: 'gray',
+});
+
+export const ratios = {
+  '16/9': style({
+    aspectRatio: '16/9',
+  }),
+  '3/4': style({
+    aspectRatio: '3/4',
+  }),
+  '1/1': style({
+    aspectRatio: '1/1',
+  }),
+};
