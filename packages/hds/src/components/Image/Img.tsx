9a26d6ef79368108b6fb9f83f235928d3d948cb0
sinamong0620 <sinaskim60@gmail.com>
2024-05-07 23:29:22 +0900

    #11 docs : type 변경, props 추가 , src 변경 가능 코드 수정
    props type 수정, props -> width,height,alt 추가, setImgSrc 추가 -> 사진 변경 가능 하게 수정

--- packages/hds/src/components/Image/Img.tsx
+++ packages/hds/src/components/Image/Img.tsx
@@ -1,31 +1,35 @@
-import React, { forwardRef, useState } from 'react';
-import { container, imageStyle, ratios } from './Image.css';
+import React, { ComponentPropsWithoutRef, forwardRef, useEffect, useState } from 'react';
+import { imageStyle, ratios } from './Image.css';
 import Image from 'next/image';
-import noImage from '../../../../../src/Images/noImage.png';
+import noImage from '../../systems/images/noImage.png';
 
-interface Image {
-  src: string;
+interface Img extends ComponentPropsWithoutRef<typeof Image> {
+  width: number;
+  height: number;
   ratio: '1/1' | '16/9' | '3/4';
 }
 
-const Img = forwardRef<HTMLImageElement, Image>(({ src, ratio }, ref) => {
+const Img = forwardRef<HTMLImageElement, Img>(({ src, ratio, alt, width, height }, ref) => {
   const [imgSrc, setImgSrc] = useState(src);
+
+  useEffect(() => {
+    setImgSrc(src);
+  }, [src]);
+
   const changeSrcFunc = () => {
     setImgSrc(noImage);
   };
 
   return (
-    <div className={container}>
-      <Image
-        ref={ref}
-        src={imgSrc}
-        alt="img"
-        width={100}
-        height={100}
-        className={`${ratios[ratio]} ${imageStyle}`}
-        onError={changeSrcFunc}
-      />
-    </div>
+    <Image
+      ref={ref}
+      src={imgSrc}
+      alt={alt}
+      width={width}
+      height={height}
+      className={`${ratios[ratio]} ${imageStyle}`}
+      onError={changeSrcFunc}
+    />
   );
 });
 
