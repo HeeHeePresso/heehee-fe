bcf35a497ec674e8a906c4859cf080a145a84773
sinamong0620 <sinaskim60@gmail.com>
2024-05-07 14:20:26 +0900

    docs : 불필요한 타입 삭제

--- packages/hds/src/components/Image/Img.stories.tsx
+++ packages/hds/src/components/Image/Img.stories.tsx
@@ -1,23 +1,23 @@
-import type {Meta,StoryFn,StoryObj} from "@storybook/react";
-import Img from "./Img";
+import type { Meta, StoryFn, StoryObj } from '@storybook/react';
+import Img from './Img';
 
-const meta:Meta<typeof Img> = {
-    title : "General/Img",
-    component: Img,
-    tags: ['autodocs'],
-    argTypes: {
-        ratio:{
-            control : "select",
-            options:["1/1","16/9","3/4"],
-        },
+const meta = {
+  title: 'General/Img',
+  component: Img,
+  tags: ['autodocs'],
+  argTypes: {
+    ratio: {
+      control: 'select',
+      options: ['1/1', '16/9', '3/4'],
     },
-    args:{
-        src:'https://picsum.photos/200/400',
-    }
+  },
+  args: {
+    src: 'https://picsum.photos/200/400',
+  },
 } satisfies Meta<typeof Img>;
 
 export default meta;
 
 type Story = StoryObj<typeof Img>;
 
-export const Default: StoryFn<typeof Img> = props => <Img {...props}/>
+export const Default: StoryFn<typeof Img> = props => <Img {...props} />;
