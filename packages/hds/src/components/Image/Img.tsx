b7a92b030304316a60509ba69d7c09411c7e093c
sinamong0620 <sinaskim60@gmail.com>
2024-05-07 14:18:17 +0900

    Comment : 주석 삭제

--- packages/hds/src/components/Image/Img.tsx
+++ packages/hds/src/components/Image/Img.tsx
@@ -1,27 +1,30 @@
-import React,{forwardRef, useState} from 'react';
+import React, { forwardRef, useState } from 'react';
 import { container, imageStyle, ratios } from './Image.css';
 import Image from 'next/image';
-import noImage from "../../../../../src/Images/noImage.png";
+import noImage from '../../../../../src/Images/noImage.png';
 
 interface Image {
   src: string;
-  ratio: "1/1" | "16/9" | "3/4";
+  ratio: '1/1' | '16/9' | '3/4';
 }
 
-const Img = forwardRef<HTMLImageElement, Image>(({src, ratio}, ref) => {
-  //이미지 비율 입력받았을때 할 것
-  // const [w,h] = ratio.split("/").map(Number);
-
-  //이미지 src를 저장하는 useState
-  const [imgSrc,setImgSrc] = useState(src);
-  //이미지 src 새로 저장하는 setState함수
+const Img = forwardRef<HTMLImageElement, Image>(({ src, ratio }, ref) => {
+  const [imgSrc, setImgSrc] = useState(src);
   const changeSrcFunc = () => {
     setImgSrc(noImage);
-  } 
+  };
 
   return (
     <div className={container}>
-      <Image ref={ref} src={imgSrc} alt="img" width={100} height={100} className={`${ratios[ratio]} ${imageStyle}`} onError={changeSrcFunc}/>
+      <Image
+        ref={ref}
+        src={imgSrc}
+        alt="img"
+        width={100}
+        height={100}
+        className={`${ratios[ratio]} ${imageStyle}`}
+        onError={changeSrcFunc}
+      />
     </div>
   );
 });
