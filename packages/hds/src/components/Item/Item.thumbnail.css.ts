1aa0a25cf3e6db3cd1574f885de0694c8e6acd36
narip <narip@github.com>
2024-05-13 22:50:28 +0900

    feat: ItemThumbnail 컴포넌트에 Image 컴포넌트 적용

--- packages/hds/src/components/Item/Item.thumbnail.css.ts
+++ packages/hds/src/components/Item/Item.thumbnail.css.ts
@@ -4,9 +4,5 @@ import globalVars from '@/styles/globalVars.css';
 export const imageCss = style({
   width: '100px',
   height: '100px',
-  objectFit: 'cover',
-  objectPosition: 'center',
-  backgroundColor: globalVars.color.beige_01,
   borderRadius: '50%',
-  display: 'inline-block',
 });
--- packages/hds/src/components/Item/Item.thumbnail.tsx
+++ packages/hds/src/components/Item/Item.thumbnail.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import { clsx } from 'clsx/lite';
+import Image from '@/components/Image';
 import * as styles from './Item.thumbnail.css';
 
 const ItemThumbnail = React.forwardRef<HTMLImageElement, React.ImgHTMLAttributes<HTMLImageElement>>(
   ({ className, ...rest }, ref) => {
     return (
-      <img
+      <Image
         ref={ref}
         width={100}
         height={100}
         className={clsx(styles.imageCss, className)}
-        onError={e => {
-          (e.target as HTMLImageElement).src =
-            'https://github.com/HeeHeePresso/heehee-fe/assets/23569208/9486b5d5-1232-4ba4-8118-0ea57ebacbab';
-        }}
         {...rest}
       />
     );
