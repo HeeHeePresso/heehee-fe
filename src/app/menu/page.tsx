1e20c63b032b86d8072e2e3842650963321493dc
narip <narip@github.com>
2024-05-15 02:38:42 +0900

    feat: 메뉴 페이지 라우팅 추가

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,13 @@
+import BottomNavUser from '@/components/BottomNav/BottomNavUser';
+import ScrollView from '@hehe/hds//components/ScrollView';
+
+export default function Menu() {
+  return (
+    <>
+      <ScrollView isFullPage topSpacing={0}>
+        메뉴 페이지
+      </ScrollView>
+      <BottomNavUser />
+    </>
+  );
+}
